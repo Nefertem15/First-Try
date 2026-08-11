f39145606d943238b2e2e5a3aa80a6dfa333819f
Joseph Ferano <joseph@ferano.io>
2026-08-11 09:05:06 +0700

    Structure things a little bit better, render 3d grid

--- Main.py
+++ Main.py
@@ -1,8 +1,25 @@
-from pyray import *
-init_window(800, 450, "Hello")
-while not window_should_close():
-    begin_drawing()
-    clear_background(WHITE)
-    draw_text("Hello world", 190, 200, 20, VIOLET)
-    end_drawing()
-close_window()
+import pyray as rl
+from pyray import (Rectangle as Rect, Vector2 as Vec2, Vector3 as Vec3, BoundingBox)
+
+rl.init_window(800, 450, "Hello")
+
+cam_pos = Vec3(0, 10, 10)
+look_target = Vec3(0, 0, 0)
+camera_up = Vec3(0, 1, 0)
+camera = rl.Camera3D(cam_pos, look_target, camera_up, 45, rl.CAMERA_PERSPECTIVE)
+
+
+while not rl.window_should_close():
+    rl.begin_drawing()
+    rl.clear_background(rl.WHITE)
+    rl.draw_text("Hello world", 190, 200, 20, rl.VIOLET)
+
+    rl.begin_mode_3d(camera)
+
+    rl.draw_grid(100, 1)
+
+    rl.end_mode_3d()
+
+    rl.end_drawing()
+
+rl.close_window()
